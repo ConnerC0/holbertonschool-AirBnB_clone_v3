6e735f29b0ce757596b994e24308aab97b13860a
Vernon-444 <mt.vernon44@gmail.com>
2022-11-27 09:30:54 -0800

    adjusted api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
-"""
-Sets up first endpoint that returns stats of the API
-"""
-
-
+"""app Module"""
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from models import storage
-from os import getenv
-
+from os import environ
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown(junk):
     """
-    Calls storage.close
+    Removes current SQLAlchemy Session.
     """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(exception):
-        """
-        404 Page Not Found Error Handler
-        """
-        return make_response(jsonify({"error": "Not found"}), 404)
+def not_found(error):
+    """
+    Return 404 error JSON string.
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = "5000"
-    if getenv("HBNB_API_HOST"):
-        host = getenv("HBNB_API_HOST")
-    if getenv("HBNB_API_PORT"):
-        port = getenv("HBNB_API_PORT")
-
-    app.run(host=host, port=port, threaded=True)
+    h = environ.get('HBNB_API_HOST', '0.0.0.0')
+    p = environ.get('HBNB_API_PORT', '5000')
+    app.run(host=h, port=p, threaded=True)
