f65a30869b05c1f1be6d91e8e0e518e15b0109cd
ConnerC0 <conner.curtis72@gmail.com>
2022-11-28 11:20:14 -0800

    cities needs reworked

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,13 @@
 """app Module"""
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-from models import storage
 from api.v1.views import app_views
+from models import storage
 from os import environ
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
