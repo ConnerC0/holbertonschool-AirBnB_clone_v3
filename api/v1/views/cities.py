d63d5658f3695afdaf37cd55bc2efa93ee4bba24
ConnerC0 <conner.curtis72@gmail.com>
2022-11-28 12:14:13 -0800

    cities updated

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,79 +5,86 @@ actions.
 """
 from api.v1.views import app_views
 from models import storage
+from models.city import City
 from models.state import State
 from flask import jsonify, abort, request, make_response
 
 
-@app_views.route('/states',
+@app_views.route('/states/<state_id>/cities',
                  methods=['GET'],
                  strict_slashes=False
                  )
-def list_states():
+def list_cities(state_id):
     """List all `State` objects"""
-    states_dict = storage.all(State)
-    states_list = []
-    for state in states_dict.values():
-        states_list.append(state.to_dict())
-    return jsonify(states_list)
+    states = storage.get(State, state_id)
+    result = []
+    if states is None:
+        abort(404)
+    for cities in states.cities:
+        result.append(cities.to_dict())
+    return jsonify(result)
+    
 
-
-@app_views.route('/states/<state_id>',
+@app_views.route('/states/<city_id>',
                  methods=['GET'],
                  strict_slashes=False
                  )
-def get_state(state_id):
+def get_city_id(city_id):
     """Retrieves a `State` object."""
-    state = storage.get(State, state_id)
-    if state is not None:
-        return jsonify(state.to_dict())
+    city = storage.get(City, city_id)
+    if city is not None:
+        return make_response(jsonify(city.to_dict()))
     abort(404)
 
 
-@app_views.route('/states/<state_id>',
+@app_views.route('/states/<city_id>',
                  methods=['DELETE'],
                  strict_slashes=False
                  )
-def delete_state(state_id):
+def delete_city(city_id):
     """Deletes a `State` object."""
-    state = storage.get(State, state_id)
-    if state is not None:
-        storage.delete(state)
+    city = storage.get(City, city_id)
+    if city is not None:
+        storage.delete(city)
         storage.save()
         return make_response(jsonify({}), 200)
     abort(404)
 
 
-@app_views.route('/states',
+@app_views.route('/states/<state_id>/cities/',
                  methods=['POST'],
                  strict_slashes=False
                  )
-def create_state():
+def create_city_post(state_id):
     """Creates a `State` object."""
-    request_dict = request.get_json(silent=True)
+    request_dict = request.get_json()
     if request_dict is not None:
         if 'name' in request_dict.keys() and request_dict['name'] is not None:
-            new_state = State(**request_dict)
-            new_state.save()
-            return make_response(jsonify(new_state.to_dict()), 201)
+            state = storage.get(State, state_id)
+            if state is None:
+                return abort(404)
+            request_dict['state_id'] = state_id
+            city = City(**request_dict)
+            city.save()
+            return make_response(jsonify(request_dict.to_dict()), 201)
         return make_response(jsonify({'error': 'Missing name'}), 400)
     return make_response(jsonify({"error": "Not a JSON"}), 400)
 
 
-@app_views.route('/states/<state_id>',
+@app_views.route('/states/<city_id>',
                  methods=['PUT'],
                  strict_slashes=False
                  )
-def update_state(state_id):
+def update_city(city_id):
     """Updates a `State` object."""
-    request_dict = request.get_json(silent=True)
+    request_dict = request.get_json()
     if request_dict is not None:
-        state = storage.get(State, state_id)
-        if state is None:
+        city = storage.get(City, city_id)
+        if city is None:
             abort(404)
         for key, val in request_dict.items():
             if key not in ['id', 'created_at', 'updated_at']:
-                setattr(state, key, val)
+                setattr(city, key, val)
         storage.save()
-        return make_response(jsonify(state.to_dict()), 200)
+        return jsonify(city.to_dict())
     return make_response(jsonify({"error": "Not a JSON"}), 400)
