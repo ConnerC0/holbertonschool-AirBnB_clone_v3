a640382aa3f5abf80fb3ba658621e645fe6eb6b4
Vernon-444 <mt.vernon44@gmail.com>
2022-11-29 08:47:17 -0800

    adjusted docstrings

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,7 +86,7 @@ class DBStorage:
         return None
 
     def count(self, cls=None):
-        """Returns the number of objects in storage
+        """Returns the number of objects in the storage
         matching the given class.
         If no class is passed, returns the count of
         all objects in storage."""
